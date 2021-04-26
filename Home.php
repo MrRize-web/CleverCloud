8850c12193889bb052ed7f243477d2b7f1bb9970
MrRize-web <54984438+MrRize-web@users.noreply.github.com>
2021-04-26 12:29:10 +0200

    OK

--- Home.php
+++ Home.php
@@ -1,8 +1,10 @@
 <html>
- <head>
-  <title>Test PHP</title>
- </head>
- <body>
- <?php echo '<p>Bonjour le monde</p>'; ?>
- </body>
-</html>
+    <head>
+        <title>Test PHP</title>
+    </head>
+<body>
+<?php
+    echo '<h1>Bonjour les gens!</h1><br>';
+?>
+</body>
+</html>>
