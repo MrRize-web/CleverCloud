ccf2456579f9d4bc579a1ecb1e3b7a1fd840a17a
MrRize-web <54984438+MrRize-web@users.noreply.github.com>
2021-04-26 13:21:08 +0200

    aa

--- index.php
+++ index.php
@@ -5,6 +5,17 @@
 <body>
 <?php
     echo '<h1>Bonjour les gens!</h1>';
+        class Voiture {
+        public $vitesse = 130;
+    }
+
+      function checkVitesse(Voiture $veh) {
+        if ($veh->vitesse > 100)
+            echo "Exces de vitesse";
+    }
+
+    $v = new Vehicule;
+    checkVitesse($v);
 ?>
 </body>
 </html>>
